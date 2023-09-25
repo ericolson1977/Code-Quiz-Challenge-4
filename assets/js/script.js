fc60bf8a3689993bf821bf3b79e54f925a8a6f11
ericolson1977 <mr_tues_night@yahoo.com>
2023-09-24 21:46:50 -0500

    added some styling to answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 // alert ("working");
-// var highScores = document.querySelector(".highscores");
-// var scores = [C:/Users/eolso/Desktop/bootcamp/Challenges/Code-Quiz-Challenge-4/assets/scores.html]
 var startQuiz = document.querySelector("#start");
 var timerP = document.querySelector("#timer");
 var boxSection = document.querySelector(".box");
@@ -33,6 +31,7 @@ var questionList = [
 var question;
 var currentIndex = 0
 
+// when use clicks start button, timer starts and quiz function is called
 startQuiz.addEventListener("click", function(){
     boxSection.style.display = "none";
     questionSection.style.display = "block";
@@ -42,20 +41,19 @@ startQuiz.addEventListener("click", function(){
             count--;
             timerP.textContent = "Time Remaining: " + count;
         }else {
-            // clearInterval(timer)
             timerP.textContent = "Game Over";
         }
     },1000)
 })
 
+// event listeners in 62-65 capture user choice and compare to actual correct answer
 function answer(event){
     var correctAnswer = questionList[currentIndex].answer
     var selectedAnswer = event.currentTarget.textContent
     if(selectedAnswer == correctAnswer){
         alert("correct")
-    
     }else{
-        alert("wring")
+        alert("wrong")
     }
     currentIndex++
     quiz()
@@ -66,22 +64,13 @@ buttonEl[1].addEventListener("click", answer);
 buttonEl[2].addEventListener("click", answer);
 buttonEl[3].addEventListener("click", answer);
 
+// this will call the first question
 function quiz(){
     question = questionList[currentIndex]
-    console.log(question);
+    // console.log(question);
     h3.textContent = question.question;
     buttonEl[0].textContent = question.choices[0];
     buttonEl[1].textContent = question.choices[1];
     buttonEl[2].textContent = question.choices[2];
     buttonEl[3].textContent = question.choices[3];
 }
-
-
-
-// var h1El = createElement("h1")
-// h1El.textContent = questions[1].question
-
-// highScores.addEventListener("click", function() {
-//     window.location.href = scores
-    
-// })
