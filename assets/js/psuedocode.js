5d45753ce0e94a063234ee5b0c72f0f52e8d401b
ericolson1977 <mr_tues_night@yahoo.com>
2023-09-26 23:41:10 -0500

    finshed code quiz, quiz cycles through questions, display final score and records that score on a highscros list

--- assets/js/psuedocode.js
+++ assets/js/psuedocode.js
@@ -45,10 +45,10 @@
 
 // Acceptance Criteria
 //     GIVEN I am taking a code quiz
-//     WHEN I click the start button  = DONE
-//     THEN a timer starts and I am presented with a question
+//     WHEN I click the start button  
+//     THEN a timer starts and I am presented with a question = DONE
 //     WHEN I answer a question
-//     THEN I am presented with another question  DONE
+//     THEN I am presented with another question = DONE
 //     WHEN I answer a question incorrectly
 //     THEN time is subtracted from the clock = DONE
 //     WHEN all questions are answered or the timer reaches 0
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var questionSection = document.querySelector(".question-section");
 var h3 = document.querySelector("#question");
 var buttonEl = document.querySelectorAll(".button");
 var scoreSection = document.querySelector(".score-section");
+var submitBtn = document.querySelector("#submit");
+var initialsEl = document.querySelector("#input-initials");
+var scoreList = document.querySelector("#score-list");
+var highScoreSection = document.querySelector(".high-score-list")
+var startOverBtn = document.querySelector("#start-over");
+var highScoresPage = document.querySelector("#high-score-link");
 var count = 30;
 var questionList = [
     {
@@ -27,11 +33,25 @@ var questionList = [
         question: "What is your Quest?",
         choices: ["To learn coding", "To find the Holy Grail", "To learn Spanish", "To learn to bake"],
         answer: "To find the Holy Grail"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
     }
 ];
 var question;
 var currentIndex = 0;
 var timer;
+var gameOver = false;
+var quizOver = false;
+
+// links to Highsocres section of page and only diplays that section
+highScoresPage.addEventListener("click", function() {
+    boxSection.style.display = "none";
+    questionSection.style.display = "none";
+    highScoreSection.style.display = "block"
+})
 
 // when use clicks start button, timer starts and quiz function is called
 startQuiz.addEventListener("click", function(){
@@ -50,9 +70,30 @@ startQuiz.addEventListener("click", function(){
     },1000)
 })
 
-var gameOver = false;
+// event listeners to capture user choice and compare to actual correct answer
+buttonEl[0].addEventListener("click", answer);
+buttonEl[1].addEventListener("click", answer);
+buttonEl[2].addEventListener("click", answer);
+buttonEl[3].addEventListener("click", answer);
 
-// this fuction allows use to select and answer, messages deiplsyed indicating correct of incorrect, and timer is decremented 10 sec forn incorret answer.
+// this will start the quiz
+function quiz(){
+    if (quizOver) {
+        questionSection.style.display = "none";
+        scoreSection.style.display = "block";
+        document.querySelector("#final-score").textContent = count;
+        localStorage.setItem("Final Score", count)
+    } else {
+        question = questionList[currentIndex]
+        h3.textContent = question.question;
+        buttonEl[0].textContent = question.choices[0];
+        buttonEl[1].textContent = question.choices[1];
+        buttonEl[2].textContent = question.choices[2];
+        buttonEl[3].textContent = question.choices[3];
+    }
+}
+
+// this fuction allows use to select and answer, messages diplayed indicating correct of incorrect, and timer is decremented 10 sec forn incorret answer.
 function answer(event){
     if(gameOver) {
         return;
@@ -67,35 +108,32 @@ function answer(event){
         count -= 10;
     }
     currentIndex++
-    
     if (currentIndex < questionList.length) {
         quiz();
     } else {
         gameOver = true;
         clearInterval(timer);
         timerP.textContent = "Score: " + count;
+        quizOver = true;
+        quiz();
     }
 }
 
-// event listeners to capture user choice and compare to actual correct answer
-buttonEl[0].addEventListener("click", answer);
-buttonEl[1].addEventListener("click", answer);
-buttonEl[2].addEventListener("click", answer);
-buttonEl[3].addEventListener("click", answer);
+//user can enter initals to have them added, with the score, to the high scores list
+submitBtn.addEventListener("click", function(e){
+    scoreSection.style.display = "none";
+    highScoreSection.style.display = "block";
+    e.preventDefault()
+    var userInitials = initialsEl.value
+    localStorage.setItem("Initials", userInitials);
+    var highScoreInitials = localStorage.getItem("Initials");
+    var highScoreScore = localStorage.getItem("Final Score");
+    var scoreListItem = document.createElement("li");
+    scoreListItem.textContent = "Initials: " + highScoreInitials + ", Score: " + highScoreScore;
+    scoreList.appendChild(scoreListItem);
+})
 
-// this will call the first question
-function quiz(){
-    question = questionList[currentIndex]
-    // console.log(question);
-    if (question) {
-        h3.textContent = question.question;
-        buttonEl[0].textContent = question.choices[0];
-        buttonEl[1].textContent = question.choices[1];
-        buttonEl[2].textContent = question.choices[2];
-        buttonEl[3].textContent = question.choices[3];
-    } else {
-        questionSection.style.display = "none";
-        scoreSection.style.display = "block";
-        document.querySelector("#final-score").textContent = "Final Score: " + count;
-    } 
-}
+//starts the quiz over
+startOverBtn.addEventListener("click", function() {
+    location.reload()
+})
